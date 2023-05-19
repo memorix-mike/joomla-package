afef0898fdc1d35de1a50fc96eadd7ec7f547990
Mike van den Berg <m.vandenberg@picturae.com>
2023-05-19 14:53:32 +0200

    WIP - Installer

--- src/Installer/Installer.php
+++ src/Installer/Installer.php
@@ -4,11 +4,50 @@ namespace PicturaeJoomla\Installer;
 
 use Composer\Composer;
 use Composer\IO\IOInterface;
+use Composer\Installer\InstallerInterface;
+use Composer\Package\PackageInterface;
+use Composer\Repository\InstalledRepositoryInterface;
 
 class Installer extends InstallerInterface
 {
     public function __construct(IOInterface $io, Composer $composer)
     {
-        var_dump('Lets go!');
+    }
+
+    public function supports(string $packageType)
+    {
+    }
+
+    public function isInstalled(InstalledRepositoryInterface $repo, PackageInterface $package)
+    {
+    }
+
+    public function download(PackageInterface $package, ?PackageInterface $prevPackage = null)
+    {
+    }
+
+    public function prepare(string $type, PackageInterface $package, ?PackageInterface $prevPackage = null)
+    {
+    }
+
+    public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
+    {
+        var_dump('lets install this plugin!');
+    }
+
+    public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target)
+    {
+    }
+
+    public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package)
+    {
+    }
+
+    public function cleanup(string $type, PackageInterface $package, ?PackageInterface $prevPackage = null)
+    {
+    }
+
+    public function getInstallPath(PackageInterface $package)
+    {
     }
 }
