9e180a015497f443a2d736e61f65a2b4acab11d8
Mike van den Berg <m.vandenberg@picturae.com>
2023-05-19 15:09:42 +0200

    WIP

--- src/Installer/Plugin.php
+++ src/Installer/Plugin.php
@@ -18,11 +18,11 @@ class Plugin implements PluginInterface
 
     public function deactivate(Composer $composer, IOInterface $io)
     {
-
+        var_dump('deactivate');
     }
 
     public function uninstall(Composer $composer, IOInterface $io)
     {
-
+        var_dump('uninstall');
     }
 }
