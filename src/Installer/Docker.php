5d646abf8196bcb277c0c38eeecfeadbf80c4e3c
Mike van den Berg <m.vandenberg@picturae.com>
2023-05-19 16:21:10 +0200

    File copy tryout

--- src/Installer/Docker.php
+++ src/Installer/Docker.php
@@ -7,7 +7,7 @@ class Docker
 
     public static function setup()
     {
-        copy(__DIR__ . 'files/docker-compose.yml', getcwd() . 'docker-compose.yml');
+        copy(__DIR__ . '/files/docker-compose.yml', getcwd() . '/docker-compose.yml');
         echo 'Copied : docker-compose.yml';
     }
 
