03da4a8fd71428233be8fb09c3350c4ccf3bf2bd
Mike van den Berg <m.vandenberg@picturae.com>
2023-05-19 15:07:25 +0200

    Console tryout

--- src/Installer/Installer.php
+++ src/Installer/Installer.php
@@ -7,6 +7,7 @@ use Composer\IO\IOInterface;
 use Composer\Installer\InstallerInterface;
 use Composer\Package\PackageInterface;
 use Composer\Repository\InstalledRepositoryInterface;
+use Symfony\Component\Console\Command\Command;
 
 class Installer implements InstallerInterface
 {
@@ -32,15 +33,20 @@ class Installer implements InstallerInterface
 
     public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
     {
-        var_dump('lets install this plugin!');
+        var_dump('install');
+        return Command::SUCCESS;
     }
 
     public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target)
     {
+        var_dump('update');
+        return Command::SUCCESS;
     }
 
     public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package)
     {
+        var_dump('uninstall');
+        return Command::SUCCESS;
     }
 
     public function cleanup(string $type, PackageInterface $package, ?PackageInterface $prevPackage = null)
