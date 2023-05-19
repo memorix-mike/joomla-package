c2b7319cd236ac6f3378a98c175d67eb0c2bc153
Mike van den Berg <m.vandenberg@picturae.com>
2023-05-19 16:49:50 +0200

    Docker try

--- src/Installer/Docker.php
+++ src/Installer/Docker.php
@@ -4,16 +4,29 @@ namespace PicturaeJoomla\Installer;
 
 class Docker
 {
+
+    private static $dockerComposeFile = 'docker-compose.yml';
+
+
     public static function setup()
     {
-        $file = __DIR__ . '/files/docker/docker-compose.yml';
+        self::copyDockerComposeFile();
+    }
+
+    private static function copyDockerComposeFile()
+    {
+        $file = __DIR__ . '/files/docker/' . self::$dockerComposeFile;
 
         if(file_exists($file)) {
-            copy($file, getcwd() . '/docker-compose.yml');
-            echo 'Done copying yml file.';
+            try {
+                copy($file, getcwd() . '/docker-compose.yml');
+                echo 'Done copying yml file.' . "\n";
+            } catch (\Exception $exception) {
+                echo $exception->getMessage();
+            }
         }
         else {
-            echo 'file does not exists at: ' . __DIR__ . '/files/docker/docker-compose.yml';
+            echo 'Docker compose file does not exists at: ' . $file . "\n";
         }
     }
 }
