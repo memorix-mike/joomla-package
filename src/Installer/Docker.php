3be4fb60368d88c304d6a8bf2f417c8d21d85132
Mike van den Berg <m.vandenberg@picturae.com>
2023-05-19 16:38:45 +0200

    File copy T&E

--- src/Installer/Docker.php
+++ src/Installer/Docker.php
@@ -6,14 +6,14 @@ class Docker
 {
     public static function setup()
     {
-        $file = __DIR__ . '/files/docker-compose.yml';
+        $file = __DIR__ . '/files/ddocker/ocker-compose.yml';
 
         if(file_exists($file)) {
             copy($file, getcwd() . '/docker-compose.yml');
             echo 'Done copying yml file.';
         }
         else {
-            echo 'file does not exists at: ' . __DIR__ . '/files/docker-compose.yml';
+            echo 'file does not exists at: ' . __DIR__ . '/files/docker/docker-compose.yml';
         }
     }
 }
