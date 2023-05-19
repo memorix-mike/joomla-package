1108f0503e675a4073b1886672f1f4f179d866b0
Mike van den Berg <m.vandenberg@picturae.com>
2023-05-19 12:42:51 +0200

    WIP - Installer tryout

--- src/Installer/Installer.php
+++ src/Installer/Installer.php
@@ -1,6 +1,9 @@
 <?php
 namespace PicturaeJoomla\Installer;
 
+use Composer\Script\Event;
+use Composer\Installer\PackageEvent;
+
 class Installer
 {
     public function __construct()
@@ -10,6 +13,15 @@ class Installer
 
     public function postPackageInstall()
     {
+        $file = fopen("testfile.txt", "w");
+        fwrite($file, "testbestand");
+    }
+
+    public static function postUpdate(Event $event)
+    {
+        $composer = $event->getComposer();
+
+        // here be dragons
         $file = fopen("testfile.txt", "w");
         fwrite($file, "testbestand");
     }
